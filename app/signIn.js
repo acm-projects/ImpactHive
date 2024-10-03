edf37ca8ec110de3fdb59a5b427b4e05dbe043ae
phooc <bjoe7382@gmail.com>
2024-10-02 21:28:50 -0500

    sign in and sign up

--- app/signIn.js
+++ app/signIn.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Alert, StyleSheet, ActivityIndicator } from 'react-native';
-import { auth } from './firebaseConfig';
+import { auth } from '../firebaseConfig';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 export default function SignUp() {
